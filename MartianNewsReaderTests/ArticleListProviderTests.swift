144fe17eee64645708f783094f9fdc97ee771b66
XinZouS <sanking.saphira@foxmail.com>
2019-02-06 21:23:02 -0500

    mdf test case

--- MartianNewsReaderTests/ArticleListProviderTests.swift
+++ MartianNewsReaderTests/ArticleListProviderTests.swift
@@ -12,9 +12,7 @@ class ArticleListProviderTests: XCTestCase {
     func testArticleDataIsTranslated() {
         let articles = [["title": "Welcome to the Test!", "body": "Or if you'd rather, check out The New York Times online.", "images": []]]
         
-        let articleListProvider = ArticleListProvider(articles: articles as [AnyObject])
-        
-        guard let article = articleListProvider.articleAtIndex(0) else { XCTFail("Article provider should return an article."); return }
+        guard let article = ArticleListProvider.shared.articleAtIndex(0) else { XCTFail("Article provider should return an article."); return }
         
         assertArticleHasMartianTitleText(article, expectedText: "Boinga to the Boinga!")
         assertArticleHasMartianBodyText(article, expectedText: "Or if boinga boinga, boinga out The New Boinga Boinga boinga.")
@@ -23,9 +21,7 @@ class ArticleListProviderTests: XCTestCase {
     func testPunctuation() {
         let articles = [["title": "Facebook plans to raise $10.6 billion in mega IPO", "body": "I'll buy that for a dollar!", "images": []]]
 
-        let articleListProvider = ArticleListProvider(articles: articles as [AnyObject])
-        
-        guard let article = articleListProvider.articleAtIndex(0) else { XCTFail("Article provider should return an article."); return }
+        guard let article = ArticleListProvider.shared.articleAtIndex(0) else { XCTFail("Article provider should return an article."); return }
 
         assertArticleHasMartianTitleText(article, expectedText: "Boinga boinga to boinga $boinga boinga in boinga IPO")
         assertArticleHasMartianBodyText(article, expectedText: "Boinga buy boinga for a boinga!")
@@ -34,9 +30,7 @@ class ArticleListProviderTests: XCTestCase {
     func testCapitalization() {
         let articles = [["title": "New DARPA report on the internet", "body": "", "images": []]]
         
-        let articleListProvider = ArticleListProvider(articles: articles as [AnyObject])
-        
-        guard let article = articleListProvider.articleAtIndex(0) else { XCTFail("Article provider should return an article."); return }
+        guard let article = ArticleListProvider.shared.articleAtIndex(0) else { XCTFail("Article provider should return an article."); return }
         
         assertArticleHasMartianTitleText(article, expectedText: "New BOINGA boinga on the boinga")
         assertArticleHasMartianBodyText(article, expectedText: "")
